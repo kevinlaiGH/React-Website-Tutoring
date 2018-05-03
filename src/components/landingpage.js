9f1d00e6a53326299421c3bf5b70d4c1abbf55f0
Kevin Lai <kevin.km.lai@gmail.com>
2018-05-04 00:35:00 +1000

    added spcial-links for landing page +  CSS

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,42 @@ class Landing extends Component {
         return (
             <div style={{ width: '100%', margin: 'auto' }}>
                 <Grid className="landing-grid">
-                    <Cell col={12}>1</Cell>
+                    <Cell col={12}>
+                        <img
+                            src="http://i.imgur.com/c7dXoMy.png"
+                            alt="avatar"
+                            className="avatar-img"
+                        />
+
+                        <div className="banner-text">
+                            <h1>Excellent High School Teacher</h1>
+                            <hr />
+                            <p> Specialise in Mathematics | Physics | Chemistry |  Science </p>
+                            <div className="social-links">
+                                {/* LinkedIn */}
+                                <a href="https://google.com" target="_blank" rel="noopener noreferrer" >
+                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
+                                </a>
+                                {/* Github */}
+                                <a href="https://google.com" target="_blank" rel="noopener noreferrer" >
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+                                {/* Github */}
+                                <a href="https://google.com" target="_blank" rel="noopener noreferrer" >
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+                                {/* Github */}
+                                <a href="https://google.com" target="_blank" rel="noopener noreferrer" >
+                                    <i className="fa fa-github-square" aria-hidden="true" />
+                                </a>
+
+
+                            </div>
+
+                        </div>
+
+
+                    </Cell>
                 </Grid>
             </div>
         )
