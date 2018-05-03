f2efbe9b758d9e56ae0ee1b9aac455630821dbd5
Kevin Lai <kevin.km.lai@gmail.com>
2018-05-03 23:43:00 +1000

    added CSS style + routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello world!</h1>
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header-color" title="Title" scroll>
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutme">About me</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
       </div>
     );
   }
